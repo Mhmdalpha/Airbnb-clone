f4a5cfce7a2db8f28c7c8da612c6efe8a3779cb7
alpha <muhammadalphaseptian@gmail.com>
2025-03-23 20:51:01 +0700

    Fix TypeScript errors in useCountries hook

--- app/hooks/useCountries.ts
+++ app/hooks/useCountries.ts
@@ -1,6 +1,15 @@
+// @ts-ignore
 import countries from "world-countries";
 
-const formatedCountries = countries.map((country) => ({
+type CountryType = {
+   cca2: string;
+   name: { common: string };
+   flag: string;
+   latlng: [number, number];
+   region: string;
+};
+
+const formatedCountries = countries.map((country: CountryType) => ({
    value: country.cca2,
    label: country.name.common,
    flag: country.flag,
@@ -10,9 +19,11 @@ const formatedCountries = countries.map((country) => ({
 
 const useCountries = () => {
    const getAll = () => formatedCountries;
+
    const getByValue = (value: string) => {
-      return formatedCountries.find((item) => item.value === value);
+      return formatedCountries.find((item: { value: string }) => item.value === value);
    };
+
    return {
       getAll,
       getByValue,
